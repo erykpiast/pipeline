88a0830f30ac3c62069d609e319345bcd91f372a
Eryk Napierała <eryk.piast@gmail.com>
2014-10-13 22:09:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+pipeline
+===============
+
+One more implementation of "Pipes and filters" pattern.
+It's designed for a browser, but should work in any JavaScript environment.
+
---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = (function() {
+
+    var util = require('util');
+    var EventEmitter = require('events').EventEmitter;
+    var extend = require('extend');
+    
+    function Pipeline() {
+        this._filters = [];
+    }
+    
+    util.inherits(Pipeline, EventEmitter);
+    
+    extend(Pipeline.prototype, {
+        destroy: function() {
+            delete this._filters;
+            
+            this.removeEventListener();
+        },
+        filter: function(filter, context) {
+            if('function' !== typeof filter) {
+                throw new TypeError('filter must be a function');
+            }
+            
+            this._filters.push({
+                fn: filter,
+                ctx: ('undefined' !== context ? context : null)
+            });
+        },
+        execute: function(data) {
+            var filters = this._filters.slice();
+            
+            while(filters.length) {
+                var filter = filters.shift();
+                
+                try {
+                    data = filter.fn.call(filter.ctx, data);
+                } catch(err) {
+                    this.emit('error', err);
+                }
+            }
+            
+            this.emit('end', data);
+            
+            return data;
+        }
+    });
+
+})();
